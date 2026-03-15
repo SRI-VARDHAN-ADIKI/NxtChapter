1bca34003b0675103678c516ae09b2aba1df5c08
SRI-VARDHAN-ADIKI <2300033389@kluniversity.in>
2026-03-15 20:06:51 +0530

    Clean slate

--- backend/routes/adaptiveQuizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { startSession, getNextQuestion } = require('../controllers/adaptiveQuizController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/start', protect, startSession);
-router.post('/next-question', protect, getNextQuestion);
-
-module.exports = router;
